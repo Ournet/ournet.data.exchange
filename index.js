877211c042826ecf1dc89b37d628536d7f53d996
Dumitru Cantea <canteadumitru@gmail.com>
2016-01-12 16:21:56 +0200

    new version: 0.2.0

--- index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = {
-  AccessService: require('./lib/db/access.js'),
-  CacheAccessService: require('./lib/db/cache_access.js'),
-  ControlService: require('./lib/db/control.js'),
-  util: require('./lib/utils.js')
-};
